b7e901a21091d85fe1b1452d925eefb0a38b468e
Okerentie Misan <Okerentiemisan@gmail.com>
2019-12-30 18:35:26 +0100

    Added logging functionality

---
+++ startup/logging.js
@@ -0,0 +1,15 @@
+const config = require('config');
+const winston = require("winston");
+require('winston-mongodb');
+require('express-async-errors');
+
+
+const db = config.get('db');
+module.exports = function () {
+    winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+    process.on('unhandledRejection', (ex) => {
+        throw ex;
+    })
+    winston.add(winston.transports.File, { filename: 'logfile.log' });
+    winston.add(winston.transports.MongoDB, { db })
+}
