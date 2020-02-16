734e0f8a8e2f906a644fb6d0c5de60070bc8833e
thepractitioner1 <Okerentiemisan@gmail.com>
2020-02-16 01:03:34 +0100

    Bug Fix: Modified User Routes and Schema

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
 const router = express.Router();
-const { User, validate } = require("../model/user");
+const _ = require('lodash');
+const { User, validate } = require("..//model/user");
+const auth = require("../middleware/auth");
 
 
 router.get('/', auth, async (req, res) => {
@@ -10,5 +12,29 @@ router.get('/', auth, async (req, res) => {
 });
 
 router.get('/me', auth, async (req, res) => {
-    const user = await (await User.findById(req.user._id)).select('-password')
+  
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
 })
+
+router.post('/createUser', async(req,res)=>{
+    const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["username", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res
+    .header("x-auth-token", token)
+    .header("access-control-expose-headers", "x-auth-token")
+    .send(_.pick(user, ["_id", "username", "email"]));
+
+})
+
+module.exports = router;
